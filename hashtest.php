42451e16b1674a2cfd2af7d9e86412ab1c9eeedf
danieljuilfs <158517157+danieljuilfs@users.noreply.github.com>
2024-02-06 21:18:48 -0500

    Delete hashtest.php

--- hashtest.php
+++
@@ -1,9 +0,0 @@
-<?php
-$input = (string)readline("Would you like your text hashed to md5 or sha1?: ");
-$text = (string)readline("Enter the phrase to be hashed: ");
-if ((strcmp($input, "md5") == 0)){
-    print("Your text hashed in md5: " . md5($input));
-}
-else {
-    print("Your text hashed in sha1: " . sha1($input)); 
-}
