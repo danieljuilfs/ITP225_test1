854e6e6c002d61d1f58212496f9a9c826dc15f69
Daniel Juilfs <dcj84944@email.vccs.edu>
2024-02-06 21:59:19 -0500

    update hashtest.php

--- ITP225HW6/hashtest.php
+++ ITP225HW6/hashtest.php
@@ -1,9 +1,16 @@
 <?php
-$input = (string)readline("Would you like your text hashed to md5 or sha1?: ");
+
+//converts the entire string to lower case and trims the whitespace on either side
+$input = trim(strtolower((string)readline("Would you like your text hashed to md5 or sha1?: ")));
+
+//reads the users string in
 $text = (string)readline("Enter the phrase to be hashed: ");
+
+//if the text of $input is exactly md5, prints out an md5 hash of the string
 if ((strcmp($input, "md5") == 0)){
     print("Your text hashed in md5: " . md5($input));
 }
+//otherwise prints out a sha1 hash of that file. 
 else {
     print("Your text hashed in sha1: " . sha1($input)); 
 }
